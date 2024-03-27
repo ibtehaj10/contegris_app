04d1dbd5dbc6c00fab5706577baa533ab19bc9fc
Hafiz Ibtehaj Samir Khan <51523656+ibtehaj10@users.noreply.github.com>
2024-03-28 02:23:36 +0500

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# Use an official Python runtime as a parent image
+FROM python:3.9-slim
+FROM pytorch/pytorch:1.8.1-cuda11.1-cudnn8-runtime
+# Set the working directory in the container
+WORKDIR /
+
+# Copy the current directory contents into the container at app
+ADD . /
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+
+# Make port 5000 available to the world outside this container
+EXPOSE 8000
+
+
+# Define environment variable to use the GPU
+ENV NVIDIA_VISIBLE_DEVICES all
+ENV NVIDIA_DRIVER_CAPABILITIES compute,utility
+
+
+# # Define environment variable
+# ENV app=app.py
+
+# Run app.py when the container launches
+CMD ["gunicorn", "--workers=4", "--threads=4", "--timeout=90", "app:app"]
+
---
+++ app.py
@@ -0,0 +1,242 @@
+from flask import Flask, request, jsonify
+import json
+# import jsonpickle
+import spacy_udpipe
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import re
+import threading
+import fasttext
+from huggingface_hub import hf_hub_download
+app = Flask(__name__)
+
+spacy_udpipe.download("ur")
+spacy_udpipe.download("en")
+
+
+VALID_TOKEN = '940809d05631dea13edae629a420fdd9'
+############################################
+def token_required(f):
+    def decorator(*args, **kwargs):
+        token = None
+        # Check if an Authorization header is part of the request
+        if 'Authorization' in request.headers:
+            # Attempt to extract the token from the header
+            token = request.headers['Authorization'].split(" ")[1]
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+        if token != VALID_TOKEN:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return f(*args, **kwargs)
+    decorator.__name__ = f.__name__
+    return decorator
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+########################## MODEL LOADING #######################
+# lang_tokenizer = AutoTokenizer.from_pretrained("mwz/LanguageDetection")
+# lang_model = AutoModelForSequenceClassification.from_pretrained("mwz/LanguageDetection").to(device)
+
+
+
+ur_sent_token = AutoTokenizer.from_pretrained("mwz/UrduClassification")
+# ur_sent_model = AutoModelForSequenceClassification.from_pretrained("mwz/UrduClassification")
+
+ru_sent_token = AutoTokenizer.from_pretrained("mwz/RomanUrduClassification")
+# ru_sent_model = AutoModelForSequenceClassification.from_pretrained("mwz/RomanUrduClassification")
+
+en_sent_token = AutoTokenizer.from_pretrained("lvwerra/distilbert-imdb")
+# en_sent_model = AutoModelForSequenceClassification.from_pretrained("lvwerra/distilbert-imdb")
+# For the language detection model (already done in your snippet)
+# lang_model = AutoModelForSequenceClassification.from_pretrained("mwz/LanguageDetection").to(device)
+
+# For sentiment models (do this right after loading each model)
+ur_sent_model = AutoModelForSequenceClassification.from_pretrained("mwz/UrduClassification").to(device)
+ru_sent_model = AutoModelForSequenceClassification.from_pretrained("mwz/RomanUrduClassification").to(device)
+en_sent_model = AutoModelForSequenceClassification.from_pretrained("lvwerra/distilbert-imdb").to(device)
+
+
+############### CLEAN TEXT ##############
+def clean_text(input_text):
+    # Define regex patterns for email, phone, URL, and website
+    email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    phone_pattern = r'((?:\+\d{2}[-\.\s]??|\d{4}[-\.\s]??)?(?:\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4}))'
+    url_pattern = r"\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s!()[]{};:'\".,<>?«»“”‘’]))"
+    website_pattern = r"(http|https)://[\w-]+(.[\w-]+)+\S*"
+
+    cleaned_text = re.sub(email_pattern, '', input_text)
+    cleaned_text = re.sub(phone_pattern, '', cleaned_text)
+    cleaned_text = re.sub(url_pattern, '', cleaned_text)
+    cleaned_text = re.sub(website_pattern, '', cleaned_text)
+
+    return cleaned_text
+
+################ MAIN FUNCTION #################################
+def analyze_text_combined(text):
+    global lang_model, ur_sent_model, ru_sent_model, en_sent_model
+    # Clean the input text
+    text = str(text)
+
+
+    if torch.cuda.is_available():
+        print("CUDA is available. GPU:", torch.cuda.get_device_name(0))
+    else:
+        print("CUDA is not available.")
+
+    # print(text)
+    cleaned_text = clean_text(text)
+    
+    
+# Move the model to the specified device
+    lang_model = lang_model.to(device)
+
+    # Prepare inputs
+    inputs = lang_tokenizer(cleaned_text, return_tensors="pt")
+
+    # Move the inputs to the same device as the model
+    inputs = {k: v.to(device) for k, v in inputs.items()}
+
+    # Perform the forward pass with the model (making sure the model and inputs are on the same device)
+    outputs = lang_model(**inputs)
+
+    # Extract the predicted language index
+    predicted_language_idx = outputs.logits.argmax().item()
+
+    languages = ["English", "Roman Urdu", "Urdu"]
+    detected_lang = languages[predicted_language_idx]
+
+    # Sentiment Analysis
+    sentiment_tokenizer = None
+    sentiment_model = None
+
+    if detected_lang == "Urdu":
+        sentiment_tokenizer = ur_sent_token
+        sentiment_model = ur_sent_model 
+    elif detected_lang == "Roman Urdu":
+        sentiment_tokenizer = ru_sent_token
+        sentiment_model = ru_sent_model
+    else:
+        sentiment_tokenizer = en_sent_token 
+        sentiment_model = en_sent_model
+
+# Assuming sentiment_tokenizer and sentiment_model are already defined
+
+# Prepare the input and move it to the device (GPU or CPU)
+    sentiment_input = sentiment_tokenizer(cleaned_text, return_tensors="pt", truncation=True, padding=True)
+    sentiment_input = {k: v.to(device) for k, v in sentiment_input.items()}
+
+    sentiment_logits = sentiment_model(**sentiment_input).logits
+    sentiment_logits = sentiment_logits.cpu()
+    sentiment_pred = sentiment_logits.argmax().item()
+
+
+    sentiment_label = ""
+    if detected_lang == "Urdu":
+        sentiment_label = "Positive" if sentiment_pred == 0 else "Negative"
+    elif detected_lang == "Roman Urdu":
+        sentiment_label = "Negative" if sentiment_pred == 0 else ("Neutral" if sentiment_pred == 1 else "Positive")
+    else:
+        sentiment_label = "Negative" if sentiment_pred == 0 else "Positive"
+
+
+    # print('detected_lang  : ',detected_lang)
+    # Verb Classification
+    spacy_models = {
+        "English": spacy_udpipe.load("en"),
+        "Roman Urdu": spacy_udpipe.load("ur"),
+        "Urdu": spacy_udpipe.load("ur"),
+    }
+
+    nlp = spacy_models.get(detected_lang)
+    if nlp is None:
+        print("Language not supported")
+        return
+
+    doc = nlp(text)
+
+    verbs = []
+    for token in doc:
+        if token.pos_ == "VERB":
+            verbs.append(token.text)
+    roman_actionable=[]
+    # Actionable Words for Roman Urdu
+    actionable_file_path = "actionable.txt"  # Update this with the correct path
+    with open(actionable_file_path, "r", encoding="utf-8") as actionable_file:
+        actionable_words_roman = [line.strip().lower() for line in actionable_file]
+        roman_actionable = actionable_words_roman
+    # print(roman_actionable)
+    #actionable_words = []
+    if detected_lang == "Roman Urdu":
+        for i in text.split(' '):
+                print(i)
+                if i in roman_actionable:
+                    # print(i)
+                    verbs.append(i)
+                # print(j)  
+                # if i == j:
+
+        
+                    # print("detect action : ",i)
+                    #verbs.append(i)
+        # verbs = actionable_words_roman
+
+    # Profanity Classification
+    profanity_file_path = "All_Profane.txt"  # Update this with the correct path
+    with open(profanity_file_path, "r", encoding="utf-8", errors="ignore") as profanity_file:
+        profanity_words = [line.strip() for line in profanity_file]
+
+    masked_text = text
+    masked_words = []
+
+
+    for word in profanity_words:
+        regex = re.compile(r"\b" + re.escape(word) + r"\b", re.IGNORECASE)
+        masked_word = get_replacement_for_swear_word(word)
+
+        if regex.search(masked_text):
+            masked_text = regex.sub(masked_word, masked_text)
+            masked_words.append({"word": word, "index": masked_text.index(masked_word)})
+        if masked_words == []:
+            pass
+        else:
+            sentiment_label = "Negative"
+
+    return {
+        "Detected Language": detected_lang,
+        "Sentiment": sentiment_label,
+        "Actionable words": verbs,
+        "Profanities": masked_words,
+    }
+
+def get_replacement_for_swear_word(word):
+    return word[:1] + "*" * (len(word) - 2) + word[-1:]
+
+
+
+
+def analyze_text_combined_wrapper(text, result_container):
+    # Call the original function and store its result
+    result = analyze_text_combined(text)
+    result_container['result'] = result
+
+
+@app.route('/predict', methods=['POST'])
+@token_required
+def predict():
+    data = request.json
+    text = data['text']
+    
+    # Container for the result from the thread
+    result_container = {}
+    
+    # Start the thread, passing the container for the result
+    prediction_thread = threading.Thread(target=analyze_text_combined_wrapper, args=(text, result_container))
+    prediction_thread.start()
+    prediction_thread.join()  # Wait for the thread to complete
+
+    # Retrieve the result from the container
+    result = result_container.get('result', 'Processing error')
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    # Create a thread for running the Flask app
+    app.run(threaded=True,host="0.0.0.0")
